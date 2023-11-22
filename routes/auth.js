b4c263b2fa2e6b44cbf69efa2d51ce3bd87ff53f
EthanHaller <ttk4ey@virginia.edu>
2023-11-21 21:42:28 -0600

    login feature

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,26 @@
-var express =  require("express");
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
+
+const { OAuth2Client } = require("google-auth-library")
 
 router.post("/google/login", async (req, res) => {
-    console.log(req.body)
-    res.send("received")
+	try {
+		const clientId = req.body.clientId
+		const jwtToken = req.body.credential
+
+		const client = new OAuth2Client(clientId)
+		const ticket = await client.verifyIdToken({
+			idToken: jwtToken,
+			audience: clientId,
+		})
+
+		const payload = ticket.getPayload()
+
+		res.json({ user: payload })
+	} catch (error) {
+		console.error("Google login error:", error)
+		res.status(500).json({ error: "Internal Server Error" })
+	}
 })
 
-module.exports = router;
+module.exports = router
