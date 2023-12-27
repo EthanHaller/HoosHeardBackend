90882e94e50befb4c6331afc0b31b23fc6c05c3a
EthanHaller <ttk4ey@virginia.edu>
2023-12-26 19:15:46 -0700

    setting up mongodb and creating schemas for users, prompts, responses, and comments with mongoose

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-// 3rd Party Modules 
-const express = require('express'); 
-const axios = require('axios');
-const cors = require("cors");
-require('dotenv/config'); 
-  
-// Local Modules 
-const authRouter = require('./routes/auth.js'); 
-  
-// Server Initialization 
-const app = express(); 
-const PORT = 8080; 
+const express = require("express")
+const connectDB = require("./db")
+const axios = require("axios")
+const cors = require("cors")
+require("dotenv/config")
+
+// Local Modules
+const authRouter = require("./routes/auth.js")
+
+// Server Initialization
+const app = express()
+const PORT = 8080
+
+connectDB()
 
 // cors configuration
-app.use(cors());
+app.use(cors())
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  next();
-});
+	res.setHeader("Access-Control-Allow-Origin", "*")
+	res.setHeader("Access-Control-Allow-Credentials", "true")
+	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE")
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+	)
+	next()
+})
 
-// Middlewares 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true }));
-  
-// Routes will be written here 
-app.use('/auth', authRouter);  
-  
-// Server Listen Along with Database  
-// connection(in case of data persistence) 
-app.listen(PORT, (error) =>{ 
-    if(!error) 
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
-    else 
-        console.log("Error occurred, server can't start", error); 
-    } 
-);
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Routes will be written here
+app.use("/auth", authRouter)
+
+// Server Listen Along with Database
+// connection(in case of data persistence)
+app.listen(PORT, (error) => {
+	if (!error) console.log("Server is Successfully Running, and App is listening on port " + PORT)
+	else console.log("Error occurred, server can't start", error)
+})
---
+++ db.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI)
+		console.log("Connected to MongoDB")
+	} catch (error) {
+		console.error("Error connecting to MongoDB:", error)
+	}
+}
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+	username: String,
+	password: String,
+	email: String,
+})
+
+const PromptSchema = new Schema({
+	text: String,
+	createdAt: { type: Date, default: Date.now() },
+})
+
+const ResponseSchema = new Schema({
+	userId: { type: ObjectId, ref: "User" },
+	promptId: { type: ObjectId, ref: "Prompt" },
+	text: "User's response to the prompt",
+	createdAt: { type: Date, default: Date.now() },
+})
+
+const CommentSchema = new Schema({
+	userId: { type: ObjectId, ref: "User" },
+	responseId: { type: ObjectId, ref: "Response" },
+	text: String,
+	createdAt: { type: Date, default: Date.now() },
+})
+
+const User = mongoose.model("User", UserSchema)
+const Prompt = mongoose.model("Prompt", PromptSchema)
+const Response = mongoose.model("Response", ResponseSchema)
+const Comment = mongoose.model("Comment", CommentSchema)
+
+module.exports = connectDB
