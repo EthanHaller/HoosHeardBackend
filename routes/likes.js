546276fde7d774d12c9468b0bf7a9bbebb11e3b1
EthanHaller <ttk4ey@virginia.edu>
2023-12-27 20:29:27 -0700

    refactor existing like logic to its own file

---
+++ routes/likes.js
@@ -0,0 +1,61 @@
+var express = require("express")
+var router = express.Router()
+
+const { User, Response, Like } = require("../db")
+
+router.get("likes")
+
+// like a response
+router.post("/like", async (req, res) => {
+	try {
+		const user = await User.findOne({ email: req.body.emailn })
+		if (!user) {
+			return res.status(404).json({ error: "User not found" })
+		}
+
+		const response = await Response.findOne({ _id: req.body.responseId })
+		if (!response) {
+			return res.status(404).json({ error: "Response not found" })
+		}
+
+		const newLike = new Like({
+			userId: user._id,
+			responseId: response._id,
+			createdAt: new Date(),
+		})
+		const savedLike = await newLike.save()
+
+		res.json({ response: savedResponse })
+	} catch (error) {
+		console.error("Error liking a response:", error)
+		res.status(500).json({ error: "Internal Server Error" })
+	}
+})
+
+// unlike a response
+router.post("/unlike", async (req, res) => {
+	try {
+		const user = await User.findOne({ email: req.body.email })
+		if (!user) {
+			return res.status(404).json({ error: "User not found" })
+		}
+
+		const response = await Response.findOne({ _id: req.body.responseId })
+		if (!response) {
+			return res.status(404).json({ error: "Response not found" })
+		}
+
+		await Like.findOneAndRemove({ userId: user._id, responseId: response._id })
+		const result = await Like.findOneAndRemove({ userId: user._id, responseId: response._id })
+		if (!result) {
+			res.json({ message: "Response not liked or already unliked" })
+		} else {
+			res.json({ message: "Response unliked successfully" })
+		}
+	} catch (error) {
+		console.error("Error unliking a response:", error)
+		res.status(500).json({ error: "Internal Server Error" })
+	}
+})
+
+module.exports = router
--- routes/response.js
+++ routes/response.js
@@ -1,7 +1,7 @@
 var express = require("express")
 var router = express.Router()
 
-const { User, Response, Like } = require("../db")
+const { User, Response } = require("../db")
 
 // get all responses
 router.get("/all", async (req, res) => {
@@ -72,58 +72,3 @@ router.post("/create", async (req, res) => {
 		res.status(500).json({ error: "Internal Server Error" })
 	}
 })
-
-// like a response
-router.post("/like", async (req, res) => {
-	try {
-		const user = await User.findOne({ email: req.body.emailn })
-		if (!user) {
-			return res.status(404).json({ error: "User not found" })
-		}
-
-		const response = await Response.findOne({ _id: req.body.responseId })
-		if (!response) {
-			return res.status(404).json({ error: "Response not found" })
-		}
-
-		const newLike = new Like({
-			userId: user._id,
-			responseId: response._id,
-			createdAt: new Date(),
-		})
-		const savedLike = await newLike.save()
-
-		res.json({ response: savedResponse })
-	} catch (error) {
-		console.error("Error liking a response:", error)
-		res.status(500).json({ error: "Internal Server Error" })
-	}
-})
-
-// unlike a response
-router.post("/unlike", async (req, res) => {
-	try {
-		const user = await User.findOne({ email: req.body.email })
-		if (!user) {
-			return res.status(404).json({ error: "User not found" })
-		}
-
-		const response = await Response.findOne({ _id: req.body.responseId })
-		if (!response) {
-			return res.status(404).json({ error: "Response not found" })
-		}
-
-		await Like.findOneAndRemove({ userId: user._id, responseId: response._id })
-		const result = await Like.findOneAndRemove({ userId: user._id, responseId: response._id })
-		if (!result) {
-			res.json({ message: "Response not liked or already unliked" })
-		} else {
-			res.json({ message: "Response unliked successfully" })
-		}
-	} catch (error) {
-		console.error("Error unliking a response:", error)
-		res.status(500).json({ error: "Internal Server Error" })
-	}
-})
-
-module.exports = router
